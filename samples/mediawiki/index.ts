6f819edb73a1d08b3be5c543ce90f49097d3c537
Arek Jaworski <arkadiusz@jaworski.pl>
2021-09-16 15:32:29 +0200

    Update samples/mediawiki/index.ts
    Co-authored-by: Budleigh Salterton <cz@signicode.com>

--- samples/mediawiki/index.ts
+++ samples/mediawiki/index.ts
@@ -3,7 +3,7 @@ import { PassThrough } from "stream";
 import EventSource = require("eventsource");
 
 const url = 'https://stream.wikimedia.org/v2/stream/recentchange';
-
+let streaming = false;.
 function init(outputStream, filter) {
   let eventSource = new EventSource(url);
 
