dec958f58b22aa6c0ce244c9c98a155393eff4bb
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-12-03 14:28:54 +0000

    Make the sequence more video ready

---
+++ samples/scraping/app.ts
@@ -0,0 +1,14 @@
+import { ReadableApp } from "@scramjet/types";
+import { scrap } from './scrap'
+import { sleep } from './utils'
+
+const app: ReadableApp<string, [string, string]> = async function* (_stream, url, selectorStr) {
+    while(true) {
+        const result = await scrap(url, selectorStr);
+        yield result + '\n'
+
+        await sleep(1000)
+    }
+}
+
+export default app;
