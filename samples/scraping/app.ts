646bfb4282f6fc2cd2d917b3c0a5ac8f53662b72
Kacper Pietrzak <kacper.pietrzak@scramjet.org>
2021-12-16 10:38:23 +0000

    Fix typo

--- samples/scraping/app.ts
+++ samples/scraping/app.ts
@@ -1,10 +1,10 @@
 import { ReadableApp } from "@scramjet/types";
-import { scrap } from './scrap'
+import { scrape } from './scrape'
 import { sleep } from './utils'
 
 const app: ReadableApp<string, [string, string]> = async function* (_stream, url, selectorStr) {
     while(true) {
-        const result = await scrap(url, selectorStr);
+        const result = await scrape(url, selectorStr);
         yield result + '\n'
 
         await sleep(5000)
--- samples/scraping/scrape.ts
+++ samples/scraping/scrape.ts
@@ -1,7 +1,7 @@
 import * as cheerio from 'cheerio';
 import axios from 'axios';
 
-export async function scrap(url: string, selectorStr: string) {
+export async function scrape(url: string, selectorStr: string) {
     const html = (await axios.get(url)).data;
     const $ = cheerio.load(html);
     const selector = $(selectorStr);
