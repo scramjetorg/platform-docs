44da7871e8c8f6bc825df4b427d7c544ec73f9c3
Jan Warchoł <jan.warchol@scramjet.org>
2022-01-07 16:40:53 +0100

    WIP minmax counter

--- samples/simple-counter-py/main.py
+++ samples/simple-counter-py/main.py
@@ -2,13 +2,15 @@ import asyncio
 from scramjet.streams import Stream
 
 class Count():
-    def __init__(self) -> None:
-        self.count = 0
+    def __init__(self, start, max) -> None:
+        self.start = start
+        self.count = start
+        self.max = max
     async def __aiter__(self):
-        while True:
-            await asyncio.sleep(1)
-            self.count += 1
+        while self.count < self.max+1:
             yield self.count
+            self.count += 1
+            await asyncio.sleep(1)
 
-def run(args):
-    return Stream.read_from(Count()).map(str)
+def run(start=0, max=10):
+    return Stream.read_from(Count(start, max)).map(str)
