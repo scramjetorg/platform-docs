538a6b2f3c2e5ee2baacd96b8db80dc02806f1ee
iaforek <arkadiusz@jaworski.pl>
2022-01-14 15:11:12 +0000

    Formatter

--- samples/discord-slack-connection/slack-read/index.ts
+++ samples/discord-slack-connection/slack-read/index.ts
@@ -14,6 +14,19 @@ type HasTopicInformation = {
 const messages: String[] = [];
 
 /**
+ * Remove application specific formatting and use internal STH tags instead.
+ * 
+ * @param {String} input Text to be formatted
+ * @returns {String}
+ */
+function formatter(input: String): String {
+    return input.replace(/\*\*(\w+)\*\*/, '<sth:b>$1</sth:b>')
+        .replace(/\*(\w+)\*/, '<sth:i>$1</sth:i>')
+        .replace(/~~(\w+)~~/, '<sth:s>$1</sth:s>')
+        .replace(/__(\w+)__/, '<sth:u>$1</sth:u>');
+}
+
+/**
  * Multi output application.
  *
  * @param {any} _stream - Dummy input stream
@@ -55,7 +68,7 @@ export = async function (_stream: any, SOCKET_MODE_TOKEN: String) {
                 );
 
                 if (msg === undefined) {
-                    ps.write({ id: event.payload.event.client_msg_id, text: event.payload.event.text, channel: event.payload.event.channel, thread_ts: event.payload.event.ts });
+                    ps.write({ id: event.payload.event.client_msg_id, text: formatter(event.payload.event.text), channel: event.payload.event.channel, thread_ts: event.payload.event.ts });
                     messages.push(event?.payload?.event?.client_msg_id);
                 }
             }
