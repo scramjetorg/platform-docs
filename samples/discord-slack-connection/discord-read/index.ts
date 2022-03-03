31cec0d011100ee215804988d0deaf0d80cf9096
Kacper Pietrzak <kacpietrzak@gmail.com>
2022-03-03 14:48:57 +0100

    Refactor slack-write.

--- samples/discord-slack-connection/discord-read/index.ts
+++ samples/discord-slack-connection/discord-read/index.ts
@@ -1,24 +1,29 @@
 import { ReadableApp, HasTopicInformation } from "@scramjet/types";
 import { PassThrough } from "stream";
-
 import { Client, Intents } from 'discord.js';
-
 import formatter from './utils';
 
-export = async function (_stream: any, token: string, topic: string) {
+const TOPIC = "messages-slack-inbound";
+
+export = async function (_stream: any, token: string) {
     const out: PassThrough & HasTopicInformation = new PassThrough({ objectMode: true });
 
     const client = new Client({
         intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
     });
 
+    
+    client.on('ready', () => {
+        console.log(`Connected to Discord API as ${client.user.tag}!`);
+    });
+    
     client.on('messageCreate', (message) => {
         out.write({ id: message.id, text: formatter(message.content), channel: message.channelId });
     });
+    
+    await client.login(token);
 
-    client.login(token);
-
-    out.topic = topic;
+    out.topic = TOPIC;
     out.contentType = "application/x-ndjson";
 
     return out;
