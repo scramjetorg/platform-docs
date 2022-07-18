e632cff2d4e77a5d1f72d6cf7ca13e8321d49ca5
iaforek <arkadiusz@jaworski.pl>
2022-07-18 17:17:28 +0000

    Working sequence

--- samples/linkedin/README.md
+++ samples/linkedin/README.md
@@ -2,6 +2,16 @@
 
 Sequence that reads CSV input (e.g. dump of companies from crunchbase), uses RapidAPI to gather additional data from LinkedIn and outputs aggregated details.
 
+## Pre-configuration
+
+Create `config.json` file with following content:
+```json
+{
+    "XRapidAPIKey": YOUR_RAPID_API_KEY_GOES_HERE
+}
+
+```
+
 ## Running
 
 Open three terminals and run the following commands:
@@ -9,8 +19,12 @@ Open three terminals and run the following commands:
 **The First Terminal:**
 
 ```bash
-# start sth
+# start sth by executing command...
 scramjet-transform-hub
+
+# ...or use script
+cd transform-hub
+yarn start -P 8000
 ```
 
 **The Second terminal**
--- samples/linkedin/index.ts
+++ samples/linkedin/index.ts
@@ -2,8 +2,77 @@ import { TransformApp } from "@scramjet/types";
 // import { DataStream } from "scramjet";
 import { PassThrough } from "stream";
 import csv from 'csv-parser';
+import axios, { AxiosRequestConfig } from 'axios';
+import * as cheerio from 'cheerio';
+import { XRapidAPIKey } from './config.json';
 
 const sleep = (timeMs: number) => new Promise(res => setTimeout(res, timeMs));
+const RATE_LIMIT_MS = 200;
+
+interface Company {
+    LinkedIn: string
+}
+
+/**
+ * Get company name from LinkedIn URL
+ * 
+ * @param {Company} company
+ * @returns {String} Company Name
+ */
+function getCompanyName(company: Company): String | null {
+    if (company.LinkedIn) {
+        const { pathname } = new URL(company.LinkedIn);
+
+        if (pathname.indexOf('/company/') !== 0) return null;
+
+        return pathname.match(/^\/company\/([a-zA-Z0-9\-]+)/)![1];
+    }
+
+    return null;
+}
+
+
+/**
+ * Get company data from Rapid API
+ * 
+ * @param {String} companyName 
+ * @returns {Object}
+ */
+async function getDataFromRapiAPI(companyName: String | null) {
+    if (companyName === null) return null;
+
+    const options = {
+        method: 'GET',
+        url: 'https://linkedin-companies-data.p.rapidapi.com/',
+        params: { vanity_name: companyName },
+        headers: {
+            'X-RapidAPI-Key': XRapidAPIKey,
+            'X-RapidAPI-Host': 'linkedin-companies-data.p.rapidapi.com',
+        },
+    };
+
+    return (await axios.request(options as AxiosRequestConfig)).data
+}
+
+/** 
+ * Get title, description, keywords and first 1000 characters from website
+ * 
+ * @param {String} url
+ * @returns {Object}
+ */
+async function scrape(url: string) {
+    if (!url) return null;
+
+    const html = (await axios.get(url)).data;
+
+    const $ = cheerio.load(html);
+    const title = $('head > title').text();
+    const description = $('meta[name="description"]').attr('content');
+    const keywords = $('meta[name="keywords"]').attr('content');
+    const body = $('body').text().substr(0, 1000);
+
+    return { title, description, keywords, body };
+}
 
 const app: TransformApp = (
     input
@@ -12,15 +81,26 @@ const app: TransformApp = (
 
     const stream = input.pipe(csv());
 
-    stream.on("data", async (data: Buffer) => {
+    stream.on("data", async (data) => {
         // Simple check which removes header if it's resent, i.e. if another CSV is getting processed AND it has header line.
         if (data["Organization Name"] === "Organization Name") return;
 
-        out.write("DATA: " + JSON.stringify(data) + "\n");
-
         // Deliberately slow down in order to not exceed API requests limits.
+        // Rate Limit 5 requests per second => 12 seconds
         stream.pause();
-        await sleep(5000);
+        await sleep(RATE_LIMIT_MS);
+
+        const companyName = getCompanyName(data);
+
+        const apiResponse = await getDataFromRapiAPI(companyName);
+        data.Employees = apiResponse?.employees_num;
+
+        const webResponse = await scrape(data.Website);
+
+        const result = { ...data, ...webResponse };
+
+        out.write(JSON.stringify(result) + "\n");
+
         stream.resume();
     })
         .on('end', () => {
